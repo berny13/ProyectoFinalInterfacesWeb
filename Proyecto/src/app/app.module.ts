d9d6bdbfd6ab3f8fdc53a24d8b799734a3d8c9f6
berny13 <bernyjimenezsanchez@gmail.com>
2021-02-12 16:10:40 +0100

    se añaden componentes al aplicativo

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './home/about/about.component';
+import { ProfileComponent } from './home/profile/profile.component';
+import { CardComponent } from './home/card/card.component';
+import { ListComponent } from './home/list/list.component';
+import { BoardComponent } from './home/board/board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ProfileComponent,
+    CardComponent,
+    ListComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
